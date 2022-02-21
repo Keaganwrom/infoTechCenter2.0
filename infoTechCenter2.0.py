7155b2a266f643c26d59016aa9f28d62af1e65a6
Keagan <02keagan.worm@gulllakecs.org>
2022-02-21 14:47:32 -0500

    import time library with sleep function and stable

--- infoTechCenter2.0.py
+++ infoTechCenter2.0.py
@@ -1,4 +1,6 @@
 # code name - Hornet
 
-print("Welcome to Hornets infoTechCenter\n")
+from time import sleep #Print to one line with time delay between prints
+
+print("Welcome to Hornets infoTechCenter\n"); sleep(1)
 print("Hornets operating system booting up")
